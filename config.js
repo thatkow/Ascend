01625d20c42548b3838fa50f327c6bcca84f471c
andrew <kow.andrew@gmail.com>
2025-10-24 13:41:47 +1100

    Config for dev

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCUKr8bmjIfICZwXDHE7xoaBbAXizJhx3I',
-  authDomain: 'anuascend-3f734.firebaseapp.com',
-  projectId: 'anuascend-3f734',
-  storageBucket: 'anuascend-3f734.firebasestorage.app',
-  messagingSenderId: '305441086779',
-  appId: '1:305441086779:web:0079c53daf3462bc82eb1e',
-  measurementId: 'G-MNT686PF8L',
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgdfP5tqB-vNrYqa9uMBAGHJP_GxXn3xE",
+  authDomain: "ascenddev-ba6d6.firebaseapp.com",
+  projectId: "ascenddev-ba6d6",
+  storageBucket: "ascenddev-ba6d6.firebasestorage.app",
+  messagingSenderId: "773912486951",
+  appId: "1:773912486951:web:cd7048190525358c845a02",
+  measurementId: "G-SM1M4RFJK0"
 };
